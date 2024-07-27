12855b9fad33c7d50f620a8c4bf8f0478a63f25a
NimnadiJayasundara <nimnadijmh.21@uom.lk>
2024-07-27 11:49:25 +0530

    Set Header, Footer and Navgation

---
+++ myportfolio/components/footer.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import Image from 'next/image'
+import { Link, animateScroll as scroll } from 'react-scroll'
+import React from 'react'
+//import Social from '@/components/social'
+
+const Footer = () => {
+  return (
+    <section className='bg-secondary py-8'>
+      <div className='container flex flex-col items-center justify-between gap-4 xl:flex-row'>
+        
+      </div>
+    </section>
+  )
+}
+
+export default Footer
