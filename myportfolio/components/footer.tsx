a2d1ebd0198c89cbeae87de9f7118b5902f1f59b
NimnadiJayasundara <nimnadijmh.21@uom.lk>
2024-08-31 23:37:50 +0530

    Update navigation bar

--- myportfolio/components/footer.tsx
+++ myportfolio/components/footer.tsx
@@ -1,18 +1,47 @@
-'use client'
-
-import Image from 'next/image'
-import { Link, animateScroll as scroll } from 'react-scroll'
-import React from 'react'
-//import Social from '@/components/social'
+import { TbBrandGithub } from "react-icons/tb";
+import { SlSocialYoutube } from "react-icons/sl";
+import {
+  SlSocialLinkedin,
+  SlSocialFacebook,
+  SlSocialInstagram,
+} from "react-icons/sl";
 
 const Footer = () => {
   return (
-    <section className='bg-secondary py-8'>
-      <div className='container flex flex-col items-center justify-between gap-4 xl:flex-row'>
-        
-      </div>
-    </section>
-  )
-}
+    <div className="hidden mdl:inline-flex xl:hidden items-center justify-center w-full py-6 gap-4">
+      <a href="https://github.com/noorjsdivs" target="_blank">
+        <span className="w-10 h-10 text-xl bg-bodyColor border-[1px] border-zinc-700 hover:border-textGreen text-zinc-200 rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
+          <TbBrandGithub />
+        </span>
+      </a>
+      <a
+        href="https://www.youtube.com/channel/UChkOsij0dhgft0GhHRauOAA"
+        target="_blank"
+      >
+        <span className="w-10 h-10 text-xl bg-bodyColor border-[1px] border-zinc-700 hover:border-textGreen text-zinc-200 rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
+          <SlSocialYoutube />
+        </span>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/noor-mohammad-ab2245193/"
+        target="_blank"
+      >
+        <span className="w-10 h-10 text-xl bg-bodyColor border-[1px] border-zinc-700 hover:border-textGreen text-zinc-200 rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
+          <SlSocialLinkedin />
+        </span>
+      </a>
+      <a href="https://www.facebook.com/Noorlalu143/" target="_blank">
+        <span className="w-10 h-10 text-xl bg-bodyColor border-[1px] border-zinc-700 hover:border-textGreen text-zinc-200 rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
+          <SlSocialFacebook />
+        </span>
+      </a>
+      <a href="https://www.instagram.com/simplenoor143/" target="_blank">
+        <span className="w-10 h-10 text-xl bg-bodyColor border-[1px] border-zinc-700 hover:border-textGreen text-zinc-200 rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
+          <SlSocialInstagram />
+        </span>
+      </a>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
